1665b12bfb5cabf3f4a218adc93afce55be019df
gvszg <zetaentranceserious@gmail.com>
2016-03-01 16:48:48 +0800

    JavaScript 套版

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,12 @@
+// common script for all pages
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require jquery-1.8.3.min
+//= require jquery.dcjqaccordion.2.7
+//= require jquery.scrollTo.min
+//= require jquery.nicescroll
+//= require jquery.sparkline
+//= require common-scripts
+//= require jquery.gritter
+//= require gritter-conf
