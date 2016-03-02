54eb651d68ab29dfa8140c7735882027f6395505
gvszg <zetaentranceserious@gmail.com>
2016-03-02 13:52:23 +0800

    表單初步套版

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -2,11 +2,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require jquery-1.8.3.min
-//= require jquery.dcjqaccordion.2.7
-//= require jquery.scrollTo.min
-//= require jquery.nicescroll
-//= require jquery.sparkline
-//= require common-scripts
-//= require jquery.gritter
-//= require gritter-conf
+//= require custom
