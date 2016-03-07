4dd9dfb95c8785fb7fc4928372531ee37867e6cd
gvszg <zetaentranceserious@gmail.com>
2016-03-07 23:57:08 +0800

    先取消圖片helper

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -10,15 +10,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
-  version :large do
-    process resize_to_fill: [600, 600]
-  end
-
+  
+  # 預設圖片尺寸
+  process resize_to_fit: [600, 600]
+  
+  # 封面圖片尺寸
   version :thumb do
     process resize_to_fill: [150,150]
   end
 
+  # 商品介紹圖片尺寸
   version :medium do
     process resize_to_fill: [400,400]
   end
