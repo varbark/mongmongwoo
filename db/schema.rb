17d0506e9eb303ea9074e02eafddae7b39d3b472
gvszg <zetaentranceserious@gmail.com>
2016-03-08 23:05:24 +0800

    修改聯絡電話資料型態為字串，修改側邊分類列圖示

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308063404) do
+ActiveRecord::Schema.define(version: 20160308144834) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 20160308063404) do
     t.string   "gender",     limit: 255
     t.string   "address",    limit: 255
     t.string   "uid",        limit: 255
-    t.integer  "phone",      limit: 4
+    t.string   "phone",      limit: 255
     t.integer  "status",     limit: 4,   default: 1
     t.datetime "deleted_at"
     t.datetime "created_at"
