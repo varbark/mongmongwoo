79ae4bd4f19b0b58c3c0343c55ad07e38b012f30
gvszg <zetaentranceserious@gmail.com>
2016-03-09 12:06:28 +0800

    Add user object url by uid

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,27 +6,47 @@ class Admin::UsersController < AdminController
 
   def index
     @users = User.recent
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @users }
+    end
   end
 
-  def show  
+  def show
+    @user = User.find(params[:id])    
+
+    # FIXME
+    # @user = User.find_by(uid: params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @user }
+    end
+  end
+
+  def show_uid
+    @user = User.find_by(uid: params[:uid])
+
+    respond_to do |format|
+      format.json { render json: @user }
+    end
   end
 
   #TODO
   def import_user
     begin
       user = User.new
-
       user.user_name = params[:user_name]
       user.real_name = params[:real_name]
       user.gender = params[:gender]
       user.phone = params[:phone]
       user.address = params[:address]
       user.uid = params[:uid]
-
       user.save!
-
-      render plain: "success"
-    rescue
+      render json: "success"
+    rescue Exception => e
+      render json: "error"
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
         # 外界POST request
         post "import_user", to: "users#import_user"
       end
+
+      # member do
+      #   get "/:uid", to: "users#check_user"
+      # end
     end
+
+    get "users/show_uid/:uid", to: "users#show_uid", as: "uid_user"
   end
 end
