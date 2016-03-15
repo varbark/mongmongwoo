8e11740bdada99bba21783779a725b216e0d30a7
gvszg <zetaentranceserious@gmail.com>
2016-03-15 14:20:38 +0800

    建立管理者訂單頁面

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,19 @@
 class Admin::OrdersController < AdminController
+  def index
+    @orders = Order.recent
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @orders, only: [:id, :user_id, :uid, :total, :is_paid, :status, :payment_method, :ship_fee, :items_price] }
+    end
+  end
+
+  def show
+    @order = Order.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @order }
+    end
+  end
 end
