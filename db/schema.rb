12223e635ff86d430276076195642220ff9cfbf0
gvszg <zetaentranceserious@gmail.com>
2016-03-17 13:32:02 +0800

    增加經緯度欄位到超商門市

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317102144) do
+ActiveRecord::Schema.define(version: 20160317102145) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -143,6 +143,8 @@ ActiveRecord::Schema.define(version: 20160317102144) do
     t.string   "phone",      limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.float    "lat",        limit: 24
+    t.float    "lng",        limit: 24
   end
 
   create_table "towns", force: :cascade do |t|
