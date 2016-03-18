b98a6f35dee483769afb01245e740e89dcfc5baf
gvszg <zetaentranceserious@gmail.com>
2016-03-18 12:53:42 +0800

    改寫APP端類別業內的商品資料API

---
+++ app/uploaders/images_uploader.rb
@@ -0,0 +1,11 @@
+class ImagesUploader < CarrierWave::Uploader::Base  
+  include CarrierWave::MiniMagick
+
+  storage :file
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+end
