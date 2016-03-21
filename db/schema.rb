a74988e7f7db79c0346f3ab8e3157997ea508800
gvszg <zetaentranceserious@gmail.com>
2016-03-21 16:48:13 +0800

    新增樣式欄位至訂單商品明細，商品API加入樣式

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160321065740) do
+ActiveRecord::Schema.define(version: 20160321083224) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 20160321065740) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.integer  "item_price",    limit: 4
+    t.string   "item_style",    limit: 255
   end
 
   add_index "order_items", ["order_id"], name: "index_order_items_on_order_id", using: :btree
