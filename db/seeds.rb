fc0b0f65e210b6560abe8c1f4949238717e287a6
gvszg <zetaentranceserious@gmail.com>
2016-03-30 15:52:21 +0800

    修改分頁功能

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,20 +12,26 @@
 #   description: ""
 # )
 
-Category.all.each do |category|
-  100.times do
-    item_amount = Item.count
-    category.items.create(
-      name: "我是商品 No.#{item_amount+1}",
-      price: (10..100).to_a.sample*10,
-      description: "#{
-        [
-          "什麼是文青？可以吃嗎？", 
-          "說穿了，其實文青和假文青就是兩群不願跟隨流行的人而已。", 
-          "這是和一位作家聊天時發現的。居然，作家沒有聽過「文青」。", 
-          "其實那樣的一群人（或兩群）出現在生活周遭，是件奇妙有趣的事。"
-        ].sample
-      }"
-    )
-  end
-end
+# Category.all.each do |category|
+#   100.times do
+#     item_amount = Item.count
+#     category.items.create(
+#       name: "我是商品 No.#{item_amount+1}",
+#       price: (10..100).to_a.sample*10,
+#       description: "#{
+#         [
+#           "什麼是文青？可以吃嗎？", 
+#           "說穿了，其實文青和假文青就是兩群不願跟隨流行的人而已。", 
+#           "這是和一位作家聊天時發現的。居然，作家沒有聽過「文青」。", 
+#           "其實那樣的一群人（或兩群）出現在生活周遭，是件奇妙有趣的事。"
+#         ].sample
+#       }"
+#     )
+#   end
+# end
+
+# Item.all.each do |item|
+#   unless item.photos.any?
+#     item.photos.create(image: "/tmp/J-bear.jpg")
+#   end
+# end
