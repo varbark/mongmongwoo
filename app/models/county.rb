66de7769a91de400cbe844fc86afb7884ddf51a4
gvszg <zetaentranceserious@gmail.com>
2016-03-30 10:59:26 +0800

    修改圖片縮放比例

--- app/models/county.rb
+++ app/models/county.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: counties
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class County < ActiveRecord::Base
   has_many :towns
   has_many :stores
--- app/models/road.rb
+++ app/models/road.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: roads
+#
+#  id         :integer          not null, primary key
+#  town_id    :integer
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Road < ActiveRecord::Base
   belongs_to :town
   has_many :stores
