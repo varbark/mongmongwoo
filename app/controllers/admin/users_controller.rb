5c2477aec27b4dd322f580ae8c2d9885d706ef41
gvszg <zetaentranceserious@gmail.com>
2016-04-04 19:55:02 +0800

    暫時恢復舊版新增用戶API

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < AdminController
   layout "admin"
 
   # 針對import_usere關掉表單token驗證
-  # skip_before_action :verify_authenticity_token, only: [:import_user]
+  skip_before_action :verify_authenticity_token, only: [:import_user]
 
   def index
     @user_page = @users = User.recent.paginate(:page => params[:page])
@@ -21,4 +21,43 @@ class Admin::UsersController < AdminController
       format.json { render json: @user }
     end
   end
+
+  def create
+    begin
+      user = User.new
+      user.user_name = params[:user_name]
+      user.real_name = params[:real_name]
+      user.gender = params[:gender]
+      user.phone = params[:phone]
+      user.address = params[:address]
+      user.uid = params[:uid]
+      user.save!
+      render json: "Successfully Create"
+    rescue Exception => e
+      render json: "Error"
+    end    
+  end
+
+  def show_uid
+    @user = User.find_by(uid: params[:uid])
+
+    respond_to do |format|
+      format.json { render json: @user }
+    end
+  end
+
+  def update
+    user = User.find_by(uid: params[:id])
+    begin
+      user.user_name = params[:user_name]
+      user.real_name = params[:real_name]
+      user.gender = params[:gender]
+      user.phone = params[:phone]
+      user.address = params[:address]
+      user.save!
+      render json: "Successfully Update"
+    rescue Exception => e
+      render json: "Error"
+    end
+  end  
 end
