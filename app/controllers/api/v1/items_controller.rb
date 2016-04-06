80aa1ebcab17b865371fdc9045221e92a564e1ce
gvszg <zetaentranceserious@gmail.com>
2016-04-06 16:02:17 +0800

    下架樣式不傳回App

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -7,6 +7,10 @@ class Api::V1::ItemsController < ApiController
   def show
     item = Item.includes(:photos, :specs).find(params[:id])
     specs = item.specs
+    on_shelf_specs = []
+    specs.each do |spec|
+      on_shelf_specs << spec if spec.status == "on_shelf"
+    end
 
     result_item = {}
     result_item[:id] = item.id
@@ -24,15 +28,21 @@ class Api::V1::ItemsController < ApiController
     result_item[:photos] = include_photos
 
     include_specs = []
-    specs.each do |spec|
-      spec_hash = {}
-      spec_hash[:id] = spec.id
-      spec_hash[:style] = spec.style
-      spec_hash[:amount] = spec.style_amount
-      spec_hash[:pic] = spec.style_pic.url
-      include_specs << spec_hash
+    
+    # specs.map {|spec| spec if spec.status == 0}
+    if on_shelf_specs.count >= 1
+      on_shelf_specs.each do |spec|
+        spec_hash = {}
+        spec_hash[:id] = spec.id
+        spec_hash[:style] = spec.style
+        spec_hash[:amount] = spec.style_amount
+        spec_hash[:pic] = spec.style_pic.url
+        include_specs << spec_hash
+      end
+      result_item[:specs] = include_specs
+    else
+      result_item[:specs] = ""
     end
-    result_item[:specs] = include_specs
     
     render json: result_item
   end
@@ -40,9 +50,13 @@ class Api::V1::ItemsController < ApiController
   def spec_info
     item = Item.includes(:photos, :specs).find(params[:id])
     specs = item.specs
+    on_shelf_specs = []
+    specs.each do |spec|
+      on_shelf_specs << spec if spec.status == "on_shelf"
+    end
 
     result_spec_infos = []
-    specs.each do |spec|
+    on_shelf_specs.each do |spec|
       spec_hash = {}
       spec_hash[:id] = spec.id
       spec_hash[:style] = spec.style
